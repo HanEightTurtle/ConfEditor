d5701b2ca24d8e677e97a439b74aec80646034ce
KiproPGUOGUO <shounuo.peng@alumni.stonybrook.edu>
2022-02-28 18:35:43 +0800

    Update ql_sample.py

--- kipro_ql/ql_sample.py
+++ kipro_ql/ql_sample.py
@@ -1,9 +1,16 @@
 
-
-from random import randint
+import re
+from random import randint,choice
 
 def get_conf():
     conf = []
+    repo = 'gys619_jdd_'
+    
     conf.append(['txsReadNum',str(randint(5,30)),'淘小说阅读数'])
 
+    with open(f'/ql/log/{repo}jx_factory_commodity','r',encoding='utf-8') as f:
+        r = f.read()
+    commodity = choice(re.findall('【(.*?)】',r))
+    conf.append(['COMMODITY_NAME',commodity,'京喜工厂商品名'])
+    
     return conf
